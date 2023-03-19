756d9612a68c646aa8904deb22f367576be162c7
zxmon-code <simonnomis.fr@gmail.com>
2023-03-19 17:25:08 +0100

    finished graphing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,52 @@
+use std::io::{BufRead, BufReader};
 use std::{fs, path::PathBuf};
+use std::fs::File;
 
 fn main() {
-    get_all_files();
-    print!("{:?}", std::env::var("HOME"))
+    let files = get_all_files();
+    let lines = get_all_line_numbers();
+    graph_all(files, lines);
+}
+
+fn get_bar_length(len: i32) -> String {
+    let mut final_string = "".to_string();
+    for i in 0..(len/10) {
+        if i%6==0 && i!=0 {
+            final_string.push('|')
+        }
+        final_string.push('#');
+    }
+    final_string
+}
+
+fn graph_all(files: Vec<String>, lines: Vec<i32>) {
+    let mut all_lengths = 0;
+    for entry in 0..files.len() {
+        let file_name: Vec<&str> = files[entry].split("/").collect();
+        print!("{} | {}", file_name[file_name.len()-1], get_bar_length(lines[entry]));
+        println!();
+        all_lengths += lines[entry];
+    }
+    println!("Average in minutes: {}", all_lengths/lines.len() as i32)
+}
+
+fn get_all_line_numbers() -> Vec<i32>{
+    let files = get_all_files();
+    let mut all_lines = vec![1];
+    all_lines.remove(0);
+    for file in files {
+        all_lines.push(get_number_of_lines_for_file(file));
+    }
+    // print!("{:?}", all_lines);
+    all_lines
+}
+
+fn get_number_of_lines_for_file(file: String) -> i32 {
+    let file = File::open(file).expect("could not open file");
+    let buffer = BufReader::new(file);
+    let line_count = buffer.lines().count() as i32;
+    // print!("{}", line_count);
+    line_count
 }
 
 fn get_all_files() -> Vec<String> {
@@ -23,8 +67,6 @@ fn get_all_files() -> Vec<String> {
         files_string.push(file.as_path().display().to_string())
     }
     files_string.retain(|f| *f != "".to_string());
-
-    println!("{:?}", files_string);
-
-    vec!["".to_string()]
+    // println!("{:?}", files_string);
+    files_string
 }
