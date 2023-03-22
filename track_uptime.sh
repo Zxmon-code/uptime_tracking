cf969488ae747568c4e2313191d70cd3c62c6ac5
zxmon-code <simonnomis.fr@gmail.com>
2023-03-22 19:35:35 +0100

    add

--- track_uptime.sh
+++ track_uptime.sh
@@ -3,8 +3,15 @@
 filename="$(date +%Y_%m_%d)"
 track_time() {
     while [[ true ]]; do
-      echo $(hyprctl activewindow | grep -o "class: \S*" | grep -o "\S*\$") >> "$HOME/uptime/${filename}"  
-      sleep 60;
+      if [[ "$(hyprctl cursorpos)" != "$(cat $HOME/uptime/$filename | tail -n10 | head -n1)" ]];
+      then
+        if [ "$(hyprctl activewindow | grep -o 'class: .*')" != "class: obsidian" ];
+        then
+          # echo $(hyprctl activewindow | grep -o "class: \S*" | grep -o "\S*\$") >> "$HOME/uptime/${filename}"  
+          echo $(hyprctl cursorpos) >> "$HOME/uptime/${filename}"
+          sleep 60;
+        fi
+      fi
     done
 }
 
